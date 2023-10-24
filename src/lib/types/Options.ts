0b1458490275c8cc2c7c8adf920c5933ca378e49
jill64 <intents.turrets0h@icloud.com>
2023-10-25 08:54:46 +0900

    feat: copied text provides as promise

--- src/lib/types/Options.ts
+++ src/lib/types/Options.ts
@@ -1,9 +1,11 @@
 export type Options = {
   /**
-   * A function that takes a `Promise` as an argument, which is resolved when the copy is complete. Executes when the copy button is clicked.
+   * The argument is a Promise that will be resolved when the copy is complete.
+   * The Promise contains the copied text.
+   * Executes when the copy button is clicked.
    * @default undefined
    */
-  onCopy?: (promise: Promise<void>) => unknown
+  onCopy?: (promise: Promise<string>) => unknown
 
   /**
    * Hide filename from the code block
