094bab09baec0d4ba1a762bb38989917de7ebcac
jill64 <intents.turrets0h@icloud.com>
2023-11-01 21:19:08 +0900

    chore: tune test e2e

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -10,6 +10,8 @@ test('smoke', async ({ page }) => {
 
   const textarea = page.getByPlaceholder('Markdown')
 
+  await textarea.clear()
+
   await textarea.fill(`
     \`\`\`js:index.js
     console.log('Hello World')
