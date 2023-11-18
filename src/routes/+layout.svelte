d4dcff83fc9d512631d9a243ae4caa2d8194aa6f
jill64 <intents.turrets0h@icloud.com>
2023-11-18 21:58:13 +0900

    restyle demo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { flip, theme, ThemeManager } from '@jill64/svelte-dark-theme'
+  import { Toaster } from '@jill64/svelte-toast'
+  import { MoonIcon, SunIcon } from 'svelte-feather-icons'
+  import GitHubLogo from './GitHubLogo.svelte'
+</script>
+
+<Toaster />
+<ThemeManager />
+<header>
+  <hgroup>
+    <h1>exmarkdown-code-utility</h1>
+    <p>
+      ❏ Code utility plugin for
+      <a href="https://github.com/ssssota/svelte-exmarkdown">
+        svelte-exmarkdown
+      </a>
+    </p>
+  </hgroup>
+  <button on:click={$flip} class="theme-button">
+    {#if $theme === 'dark'}
+      <SunIcon />
+    {:else}
+      <MoonIcon />
+    {/if}
+  </button>
+  <a href="https://github.com/jill64/exmarkdown-code-utility">
+    <GitHubLogo />
+  </a>
+</header>
+<slot />
+
+<style>
+  :global(body) {
+    font-family: sans-serif;
+    margin: 0 1.5rem;
+  }
+  :global(.dark body) {
+    background: #222;
+    color: #eee;
+  }
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+  }
+  a {
+    color: royalblue;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  .theme-button {
+    background: inherit;
+    color: inherit;
+    border-radius: 9999rem;
+    padding: 0.5rem;
+    cursor: pointer;
+    margin-left: auto;
+    border: none;
+  }
+  :global(.dark .theme-button:hover) {
+    background: rgba(255, 255, 255, 0.1);
+  }
+  :global(.light .theme-button:hover) {
+    background: rgba(0, 0, 0, 0.1);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { codeUtility } from '$lib/index.js'
   import { TextArea } from '@jill64/svelte-input'
-  import { toast, Toaster } from '@jill64/svelte-toast'
+  import { toast } from '@jill64/svelte-toast'
   import { Markdown } from 'svelte-exmarkdown'
   import { gfmPlugin } from 'svelte-exmarkdown/gfm'
   import mock from './mock.md?raw'
@@ -9,18 +9,14 @@
   let md = mock
 </script>
 
-<Toaster />
-<header>
-  <h1>exmarkdown-code-utility</h1>
-  <a href="https://github.com/jill64/exmarkdown-code-utility">GitHub</a>
-</header>
 <main>
   <TextArea
     placeholder="Markdown"
-    style="background: inherit; color: inherit; width: 100%; max-width: 600px; min-width: 300px;"
+    padding="0.5rem"
+    style="background: inherit; color: inherit; border-radius: 0.5rem; border: 1px solid #aaa;"
     bind:value={md}
   />
-  <div data-testid="markdown-preview">
+  <output data-testid="markdown-preview">
     <Markdown
       {md}
       plugins={[
@@ -35,29 +31,25 @@
         })
       ]}
     />
-  </div>
+  </output>
 </main>
 
 <style>
   main {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    grid-gap: 1rem;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr 1fr;
   }
-  div {
-    width: 100%;
-    min-width: 300px;
-    max-width: 600px;
-  }
-  :global(body) {
-    font-family: sans-serif;
-  }
-  @media (prefers-color-scheme: dark) {
-    :global(body) {
-      background: #222;
-      color: #eee;
+  @media (max-width: 640px) {
+    main {
+      grid-template-columns: 1fr;
     }
+  }
+  output {
+    border: 1px solid #aaa;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    overflow-x: auto;
   }
   :global(.exmarkdown-code-filename) {
     font-size: large;
@@ -78,25 +70,10 @@
   :global(.exmarkdown-code-copy):active {
     background: rgba(0, 0, 0, 0.2);
   }
-  @media (prefers-color-scheme: dark) {
-    :global(.exmarkdown-code-copy):hover {
-      background: rgba(255, 255, 255, 0.1);
-    }
-    :global(.exmarkdown-code-copy):active {
-      background: rgba(255, 255, 255, 0.2);
-    }
+  :global(.dark .exmarkdown-code-copy):hover {
+    background: rgba(255, 255, 255, 0.1);
   }
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 0.5rem;
-  }
-  header a {
-    color: inherit;
-    text-decoration: none;
-  }
-  header a:hover {
-    text-decoration: underline;
+  :global(.dark .exmarkdown-code-copy):active {
+    background: rgba(255, 255, 255, 0.2);
   }
 </style>
