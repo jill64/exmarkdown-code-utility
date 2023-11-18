52fe7b0a97e2099013650c38281ef0c163dfed13
jill64 <intents.turrets0h@icloud.com>
2023-11-18 20:19:57 +0900

    embed SENTRY_DSN

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,8 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
 
-const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
+const { onHandle, onError } = serverInit(
+  'https://9d63d41176bf79e3b810b2d19f3cc3fd@o4505814639312896.ingest.sentry.io/4506168386650112'
+)
 
 export const handle = onHandle()
 export const handleError = onError()
