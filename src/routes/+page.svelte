880557f95b6b4e8db7bdf3227e4d864a6e344f8e
jill64 <intents.turrets0h@icloud.com>
2023-11-20 01:10:35 +0900

    chore: add editable option in demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
+  import { page } from '$app/stores'
   import { codeUtility } from '$lib/index.js'
   import { theme } from '@jill64/svelte-dark-theme'
-  import { TextArea } from '@jill64/svelte-input'
+  import { TextArea, ToggleSwitch } from '@jill64/svelte-input'
   import { toast } from '@jill64/svelte-toast'
   import { Markdown } from 'svelte-exmarkdown'
   import { gfmPlugin } from 'svelte-exmarkdown/gfm'
   import github from 'svelte-highlight/styles/github'
   import githubDark from 'svelte-highlight/styles/github-dark'
+  import { define } from 'svelte-qparam'
+  import { boolean } from 'svelte-qparam/converter'
   import mock from './mock.md?raw'
+
+  const qparam = define({
+    highlight: boolean,
+    copy_button: boolean,
+    filename: boolean
+  })
+
+  $: ({ qparams } = qparam($page.url))
+  $: ({ highlight, copy_button, filename } = qparams)
 
   let md = mock
 </script>
 
 <svelte:head>
-  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-  {@html $theme === 'dark' ? githubDark : github}
+  {#if highlight}
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html $theme === 'dark' ? githubDark : github}
+  {/if}
 </svelte:head>
 
 <main>
@@ -24,23 +38,38 @@
     style="background: inherit; color: inherit; border-radius: 0.5rem; border: 1px solid #aaa;"
     bind:value={md}
   />
-  <output data-testid="markdown-preview">
-    <Markdown
-      {md}
-      plugins={[
-        gfmPlugin(),
-        codeUtility({
-          highlight: true,
-          onCopy: (promise) =>
-            $toast.promise(promise, {
-              loading: 'Copying...',
-              success: 'Copied!',
-              error: 'Failed to copy'
-            })
-        })
-      ]}
-    />
-  </output>
+  <div class="output">
+    <aside>
+      <ToggleSwitch value={$highlight} onChange={highlight.set}>
+        <span style:margin-left="0.5rem">Highlight</span>
+      </ToggleSwitch>
+      <ToggleSwitch value={$copy_button} onChange={copy_button.set}>
+        <span style:margin-left="0.5rem">Copy Button</span>
+      </ToggleSwitch>
+      <ToggleSwitch value={$filename} onChange={filename.set}>
+        <span style:margin-left="0.5rem">File Name</span>
+      </ToggleSwitch>
+    </aside>
+    <output data-testid="markdown-preview">
+      <Markdown
+        {md}
+        plugins={[
+          gfmPlugin(),
+          codeUtility({
+            highlight: $highlight,
+            hideCopyButton: !$copy_button,
+            hideFilename: !$filename,
+            onCopy: (promise) =>
+              $toast.promise(promise, {
+                loading: 'Copying...',
+                success: 'Copied!',
+                error: 'Failed to copy'
+              })
+          })
+        ]}
+      />
+    </output>
+  </div>
 </main>
 
 <style>
@@ -54,10 +83,18 @@
       grid-template-columns: 1fr;
     }
   }
-  output {
+  .output {
     padding: 0.5rem;
-    border-radius: 0.5rem;
     overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  aside {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 1rem;
   }
   :global(.exmarkdown-code-filename) {
     font-size: large;
