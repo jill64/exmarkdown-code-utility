2d262c450e56f3c79c338c98f4d90052977aa7c9
jill64 <intents.turrets0h@icloud.com>
2023-12-04 18:01:25 +0900

    chore: refactor ci config

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
-import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
-import { onRender } from '@jill64/svelte-dark-theme'
+import { init } from '@jill64/sentry-sveltekit-cloudflare/server'
+import { onRender } from '@jill64/svelte-suite'
 
-const { onHandle, onError } = serverInit(
+const { onHandle, onError } = init(
   'https://9d63d41176bf79e3b810b2d19f3cc3fd@o4505814639312896.ingest.sentry.io/4506168386650112'
 )
 
